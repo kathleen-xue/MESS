4ac06e4c15c2281ace41446b2688823a614411a5
Kathleen Xue <kxue@usc.edu>
2018-09-13 03:56:22 -0700

    i am dying

--- MESS.cpp
+++ MESS.cpp
@@ -47,21 +47,28 @@ struct Network {
 	}
 };*/
 
+int costToCharge(int start, int end) {
+	return (end - start)*200;
+}
+
+int costToStay(int M, int T, int start, int end) {
+	return  -1*(start - end)*200;
+}
+
 vector<vector<vector<int> > > cheapestPath(int T, int M) {
 
-	vector<vector<int> > endCharges;
-	for(int i = 0; i < T; i++) {
+	vector<vector<int> > transportCost;
+	for(int i = 0; i < M+1; i++) {
 		vector<int> curr;
-		for(int j = 0; j < M; j++) {
-			int ec = 0;
-			cout << "What is Microgrid " << j << "'s end charge on day " << i << "? " << endl;
-			cin >> ec;
-			curr.push_back(ec);
+		for(int j = 0; j < M+1; j++) {
+			if(i == j) curr.push_back(0);
+			else {
+				curr.push_back(abs(i-j)*30);
+			}
 		}
-		endCharges.push_back(curr);
+		transportCost.push_back(curr);
 	}
 
-	int transportCost = 50;
 	vector<vector<vector<int> > > dpCUBE;
 
 	for(int i = 0; i < T; i++) {
@@ -69,12 +76,16 @@ vector<vector<vector<int> > > cheapestPath(int T, int M) {
 		for(int j = 0; j < M; j++) {
 			vector<int> curr2;
 			for(int k = 0; k < 100; k += 25) {
-				curr2.push_back(INT_MAX);
+				curr2.push_back(1000000);
+				//cout << curr2[k/25] << " ";
 			}
+			//cout << endl;
 			curr1.push_back(curr2);
 		}
 		dpCUBE.push_back(curr1);
 	}
+
+cout << "hi" << endl;
 
 	for(int i = 0; i < M; i++) {
 		for(int j = 0; j <= 100; j += 25) {
@@ -89,9 +100,9 @@ vector<vector<vector<int> > > cheapestPath(int T, int M) {
 	        for(int m = 0; m <= 100; m += 25) {
 	          for(int n = m; n <= 100; n += 25) {
 	             dpCUBE[i][j][k] = min(dpCUBE[i][j][k], dpCUBE[i-1][l][m] + costToCharge(m, n) + 
-	                2*transportCost + costToStay(j, i, n, k));
+	                transportCost[j][M+1] + transportCost[l][M+1] + costToStay(j, i, n, k));
 	             if(n == m) {
-	                dpCUBE[i][j][k] = min(dpCUBE[i][j][k], (dpCUBE[i-1][l][m] + transportCost));
+	                dpCUBE[i][j][k] = min(dpCUBE[i][j][k], (dpCUBE[i-1][l][m] + transportCost[j][l]));
                  }
               }
             }
@@ -102,15 +113,6 @@ vector<vector<vector<int> > > cheapestPath(int T, int M) {
 
 	return dpCUBE;
 }
-
-int costToCharge(int start, int end) {
-	return (end - start)*200;
-}
-
-int costToStay(int M, int T, int start, int end) {
-	return  -1*(start - end)*200;
-}
-
 
 int main () {
 	int T = 0;
