b44a61b93476cad294186e21d5f1ef84e701eed1
Kathleen Xue <kxue@usc.edu>
2018-10-02 00:18:02 -0700

    commented

--- MESS.cpp
+++ MESS.cpp
@@ -55,7 +55,7 @@ int costToStay(int M, int T, int start, int end) {
 	return  -1*(start - end)*200;
 }
 
-vector<vector<vector<int> > > cheapestPath(int T, int M) {
+vector<vector<vector<long long> > > cheapestPath(int T, int M) {
 
 	vector<vector<int> > transportCost;
 	for(int i = 0; i < M+1; i++) {
@@ -69,14 +69,14 @@ vector<vector<vector<int> > > cheapestPath(int T, int M) {
 		transportCost.push_back(curr);
 	}
 
-	vector<vector<vector<int> > > dpCUBE;
+	vector<vector<vector<long long> > > dpCUBE;
 
 	for(int i = 0; i < T; i++) {
-		vector<vector<int> > curr1; 
+		vector<vector<long long> > curr1; 
 		for(int j = 0; j < M; j++) {
-			vector<int> curr2;
+			vector<long long> curr2;
 			for(int k = 0; k <= 100; k+=25) {
-				curr2.push_back(1000000);
+				curr2.push_back(INT_MAX);
 				//cout << curr2[k/25] << " ";
 			}
 			//cout << endl;
@@ -93,15 +93,15 @@ vector<vector<vector<int> > > cheapestPath(int T, int M) {
 	}
 
 	
-	for(int i = 1; i < 2*T; i++) {
-		for(int j = 1; j < M; j++) {
-			for(int k = 0; k <= 100; k+=25) {
-			   	for(int l = 0; l < M; l++) {
-			    	for(int m = 0; m <= 100; m+=25) {
-			          	for(int n = m; n <= 100; n+=25) {
-			             	dpCUBE[i][j][k/25] = min(dpCUBE[i][j][k/25], dpCUBE[i-1][l][m/25] + costToCharge(m, n) + 
-			                transportCost[j][M] + transportCost[M][l] + costToStay(j, i, n, k));
-			             	if(n == m) { 
+	for(int i = 1; i < T; i++) { //cycling through each day
+		for(int j = 1; j < M; j++) { //cycling through the microgrids of current day
+			for(int k = 0; k <= 100; k+=25) { //cycling through each of the charge states of each microgrid of current day
+			   	for(int l = 0; l < M; l++) { //cycling through each microgrid of previous day
+			    	for(int m = 0; m <= 100; m+=25) { //cycling through each of the charge states of previous day
+			          	for(int n = m; n <= 100; n+=25) { //cycling through each of the *possible* charges of current day
+			             	if(n != m) dpCUBE[i][j][k/25] = min(dpCUBE[i][j][k/25], dpCUBE[i-1][l][m/25] + costToCharge(m, n) + 
+			                								transportCost[j][M] + transportCost[M][l] + costToStay(j, i, n, k));
+			             	else { 
 			                	dpCUBE[i][j][k/25] = min(dpCUBE[i][j][k/25], (dpCUBE[i-1][l][m/25] + transportCost[j][l]));
 		                 	}
 		              	}
@@ -122,7 +122,7 @@ int main () {
 	cout << "Enter # microgrids: " << endl;
 	cin >> M;
 
-	vector<vector<vector<int> > > dpCUBE = cheapestPath(T, M);
+	vector<vector<vector<long long> > > dpCUBE = cheapestPath(T, M);
 
 	return 0;
 }
